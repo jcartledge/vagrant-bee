2672f4d7c9aa65c089c0a4e3ead926e9c4ab85ee
James Cartledge <jcartledge@gmail.com>
2013-07-24 13:30:21 +1000

    runs bee now

--- lib/vagrant-bee/command.rb
+++ lib/vagrant-bee/command.rb
@@ -6,10 +6,24 @@ module VagrantPlugins
           o.banner = "Usage: vagrant bee [command]"
           o.separator ""
         end
+        ret = 0
         argv = parse_options(opts)
-        puts argv
-        puts @env
-        0
+        with_target_vms do |machine|
+          guest_path = machine.config.vm.synced_folders.select {|k,v|
+            v[:id] == "vagrant-root"
+          }.values[0][:guestpath]
+          cmd = "cd #{guest_path} && bee " << argv.join(" ")
+          ret += machine.communicate.execute cmd do |type, data|
+            case type
+            when :stdout
+              @env.ui.info(data, :new_line => false)
+            when :stderr
+              @env.ui.error(data, :new_line => false)
+            end
+          end
+          break if ret > 0
+        end
+        ret
       end
     end
   end
